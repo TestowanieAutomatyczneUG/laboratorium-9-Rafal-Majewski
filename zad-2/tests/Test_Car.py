b8c755f8129f545d11c053a52b691ee9c9260127
Rafał Majewski <goodheropl@gmail.com>
2021-12-01 09:27:47 +0100

    k

--- zad-2/tests/Test_Car.py
+++ zad-2/tests/Test_Car.py
@@ -2,8 +2,6 @@ from modules.Car import Car
 from unittest.mock import Mock
 import unittest
 
-# test the Car class
-
 class Test_Car(unittest.TestCase):
 	def test_needsFuel(self):
 		car = Car()
@@ -11,10 +9,11 @@ class Test_Car(unittest.TestCase):
 		car.needsFuel.return_value = True
 		self.assertTrue(car.needsFuel())
 	def test_getEngineTemperature(self):
+		engineTemperature = 100
 		car = Car()
 		car.getEngineTemperature = Mock(name = "getEngineTemperature")
-		car.getEngineTemperature.return_value = 100
-		self.assertEqual(car.getEngineTemperature(), 100)
+		car.getEngineTemperature.return_value = engineTemperature
+		self.assertEqual(car.getEngineTemperature(), engineTemperature)
 	def test_driveTo(self):
 		destination = "Brazil"
 		car = Car()
